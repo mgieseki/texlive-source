cea70a45a5e18dfb5ae3eff885bb3cb69c0a8657
Akira Kakuto <kakuto@fuk.kindai.ac.jp>
2024-11-21 23:11:23 +0000

    sync (v1.0.6)
    git-svn-id: svn://tug.org/texlive/trunk/Build/source@72927 c570f23f-e606-0410-a88d-b1316a301751

--- texk/texlive/linked_scripts/extractbb/extractbb.lua
+++ texk/texlive/linked_scripts/extractbb/extractbb.lua
@@ -7,7 +7,7 @@
 -- A wrapper script to allow you to choose which implementation of extractbb to
 -- use. Should hopefully be replaced with the ``scratch'' file in TeX Live 2025.
 --
--- v1.0.5 (2024-11-21) %%version %%dashdate
+-- v1.0.6 (2024-11-21) %%version %%dashdate
 
 ---------------------
 --- Configuration ---
@@ -31,6 +31,7 @@ local function error(...)
     end
 
     -- Flush and exit
+    io.stderr:write("\n")
     io.stderr:flush()
     os.exit(1)
 end
@@ -47,11 +48,32 @@ if (env_mode == "file") or (env_mode == "link") then
     return os.exec(arg)
 end
 
--- Map the choice names to file names.
+-- Find the subscripts
 kpse.set_program_name("texlua", "extractbb")
+
+local function find_script(name)
+    -- Find the script, searching **only** in the scripts directories.
+    local path = kpse.lookup(
+        name,
+        { path = kpse.var_value("TEXMFSCRIPTS"), format = "lua" }
+    )
+
+    -- Make sure that the script is not writable.
+    if kpse.out_name_ok_silent_extended(path) then
+        if os.env["TEXLIVE_EXTRACTBB_UNSAFE"] == "unsafe" then
+            -- If we're running in development mode, then we can allow this.
+        else
+            error("Refusing to run a writable script.")
+        end
+    end
+
+    return path
+end
+
+-- Map the choice names to file names.
 local choice_mapping = {
-    wrapper = kpse.find_file("extractbb-wrapper.lua", "lua", true),
-    scratch = kpse.find_file("extractbb-scratch.lua", "lua", true),
+    wrapper = find_script("extractbb-wrapper.lua"),
+    scratch = find_script("extractbb-scratch.lua"),
 }
 
 -- Choose the implementation to run.
@@ -59,29 +81,6 @@ local choice = choice_mapping[env_choice] or choice_mapping[DEFAULT]
 
 if not choice then
     error("No implementation of extractbb found.")
-end
-
--- Make sure that the script is not writable.
-if kpse.out_name_ok_silent_extended(choice) then
-    if os.env["TEXLIVE_EXTRACTBB_UNSAFE"] == "unsafe" then
-        -- If we're running in development mode, then we can allow this.
-    else
-        error("Refusing to run a writable script.")
-    end
-end
-
--- Make sure that the script is beside this one, just to be safe
-local split_dir_pattern = "^(.*)[/\\]([^/\\]-)$"
-local current_dir, current_name = arg[0]:match(split_dir_pattern)
-local choice_dir, choice_name = choice:match(split_dir_pattern)
-
-if current_dir ~= choice_dir then
-    -- Resolve the symlinks and try again
-    current_dir = lfs.symlinkattributes(current_dir, "target")
-    choice_dir = lfs.symlinkattributes(choice_dir, "target")
-    if current_dir ~= choice_dir then
-        error("Refusing to run a script from a different directory.")
-    end
 end
 
 -- And run it.
